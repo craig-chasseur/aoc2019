0691f9ced6d9afbb5ea19f05af4fc99428e6e4d4
Craig Chasseur <spoonboy42@gmail.com>
2019-12-04 23:19:48 -0800

    Day 4 part 0 solution

---
+++ cc/puzzle_4_0/main.cc
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+#include <utility>
+
+#include "cc/util/check.h"
+
+namespace {
+
+class Decimal {
+ public:
+  explicit Decimal(std::string rep) : rep_(std::move(rep)) {
+    CHECK(rep_.size() == 6);
+  }
+
+  bool operator==(const Decimal& other) const {
+    return rep_ == other.rep_;
+  }
+
+  bool operator!=(const Decimal& other) const {
+    return rep_ != other.rep_;
+  }
+
+  Decimal& operator++() {
+    for (auto digit_it = rep_.rbegin(); digit_it != rep_.rend(); ++digit_it) {
+      if (*digit_it < '9') {
+        ++(*digit_it);
+        return *this;
+      }
+      *digit_it = '0';
+    }
+    // Overflow.
+    CHECK(false);
+  }
+
+  bool MatchesRule() const {
+    bool some_double = false;
+    for (auto digit_it = rep_.begin(); digit_it != rep_.end() - 1; ++digit_it) {
+      if (*digit_it > *(digit_it + 1)) return false;
+      some_double = some_double || (*digit_it == *(digit_it + 1));
+    }
+    return some_double;
+  }
+
+ private:
+  std::string rep_;
+};
+
+}  // namespace
+
+int main(int argc, char** argv) {
+  if (argc != 3) {
+    std::cerr << "USAGE: main MINIMUM MAXIMUM\n";
+    return 1;
+  }
+
+  Decimal min(argv[1]);
+  Decimal max(argv[2]);
+  int matches = 0;
+  for (; min != max; ++min) {
+    matches += static_cast<int>(min.MatchesRule());
+  }
+  std::cout << matches << "\n";
+  return 0;
+}
